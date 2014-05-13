4f4118543d4c94c50c71d5b72cfac91b2dcb6128
Carlos Hugo (Fundar) <caarloshugo@gmail.com>
2014-05-13 13:10:02 -0500

    elimamdndo webhook

--- gitpull.php
+++
@@ -1,8 +0,0 @@
-<?php
-$sh_out  = exec('git pull origin master');
-exec('/usr/bin /var/www/curul501/gitpull.sh');
-$data    = array("response" => true, "sh" => $sh_out, "sh2" => $sh_out2);
-
-echo json_encode($data);
-
-//test 123
