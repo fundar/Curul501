15bd46f2059bc86eb0cfe9aaaea0c60fb1da466c
Carlos Hugo (Fundar) <caarloshugo@gmail.com>
2014-05-13 12:59:36 -0500

    git pull

--- gitpull.php
+++ gitpull.php
@@ -1,7 +1,8 @@
 <?php
-$sh_out = exec('git pull origin master');
-$data   = array("response" => true, "sh" => $sh_out);
-
+$sh_out  = exec('git pull origin master');
+$sh_out2 = exec('git pull');
+$data    = array("response" => true, "sh" => $sh_out, "sh2" => $sh_out2);
 
 echo json_encode($data);
 
+//test 2
