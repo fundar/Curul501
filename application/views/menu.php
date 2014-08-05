45e40d792e426ad41a109a135edd9c493277bbf7
Carlos Hugo (Fundar) <caarloshugo@gmail.com>
2014-08-04 19:01:07 -0500

    cambios de orden en el menu

--- application/views/menu.php
+++ application/views/menu.php
@@ -1,15 +1,15 @@
 <div>
-	<a href="<?php echo site_url('admin/political_parties')?>">
-		<?php if($this->uri->segment(2) == "political_parties") { ?><strong>Partidos políticos</strong><?php } else { ?>Partidos políticos<?php } ?>
-	</a> |
-	<a href="<?php echo site_url('admin/legislatures')?>">
-		<?php if($this->uri->segment(2) == "legislatures") { ?><strong>Legislaturas</strong><?php } else { ?>Legislaturas<?php } ?>
-	</a> |
 	<a href="<?php echo site_url('admin/representatives')?>">
 		<?php if($this->uri->segment(2) == "representatives") { ?><strong>Representantes</strong><?php } else { ?>Representantes<?php } ?>
 	</a> |
 	<a href="<?php echo site_url('admin/initiatives')?>">
 		<?php if($this->uri->segment(2) == "initiatives") { ?><strong>Iniciativas</strong><?php } else { ?>Iniciativas<?php } ?>
+	</a> |
+	<a href="<?php echo site_url('admin/political_parties')?>">
+		<?php if($this->uri->segment(2) == "political_parties") { ?><strong>Partidos políticos</strong><?php } else { ?>Partidos políticos<?php } ?>
+	</a> |
+	<a href="<?php echo site_url('admin/legislatures')?>">
+		<?php if($this->uri->segment(2) == "legislatures") { ?><strong>Legislaturas</strong><?php } else { ?>Legislaturas<?php } ?>
 	</a> |
 	<?php if(isset($_SESSION['user_id'])) { ?>
 		<a href="<?php echo site_url('admin/logout')?>">Cerrar sesión</a>
