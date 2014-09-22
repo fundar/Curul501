7be9c85fad42fe7e2254ea8fb04f509b52bae593
Carlos Hugo Gonzalez (Fundar) <carlos@fundar.org.mx>
2014-09-22 16:03:46 -0500

    relations  model postgres #debug

--- application/models/grocery_crud_generic_model.php
+++ application/models/grocery_crud_generic_model.php
@@ -129,6 +129,7 @@ class grocery_CRUD_Generic_Model  extends grocery_CRUD_Model  {
     protected function relation_n_n_queries($select)
     {
     	$this_table_primary_key = $this->get_primary_key();
+    	die(var_dump($$this_table_primary_key));
     	foreach($this->relation_n_n as $relation_n_n)
     	{
     		list($field_name, $relation_table, $selection_table, $primary_key_alias_to_this_table,
@@ -399,8 +400,6 @@ class grocery_CRUD_Generic_Model  extends grocery_CRUD_Model  {
     }
 
     function build_relation_n_n_subquery($field, $selection_table, $relation_table, $primary_key_alias_to_selection_table, $primary_key_selection_table, $primary_key_alias_to_this_table, $field_name){
-        
-        die(var_dump($primary_key_selection_table));
         return "(SELECT GROUP_CONCAT(DISTINCT ".$this->protect_identifiers($field).") FROM ".$this->protect_identifiers($selection_table)
                     ." LEFT JOIN ".$this->protect_identifiers($relation_table)." ON ".$this->protect_identifiers($relation_table.".".$primary_key_alias_to_selection_table)." = ".$this->protect_identifiers($selection_table.".".$primary_key_selection_table)
                     ." WHERE ".$this->protect_identifiers($relation_table.".".$primary_key_alias_to_this_table)." = ".$this->protect_identifiers($this->table_name.".".$this->get_primary_key($this->table_name))." GROUP BY ".$this->protect_identifiers($relation_table.".".$primary_key_alias_to_this_table).") AS ".$this->protect_identifiers($field_name);
