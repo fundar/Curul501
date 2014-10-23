c0bcdf8e9d9cdfc782e8327e7cd22fbd87fd3aed
Carlos Hugo Gonzalez (Fundar) <carlos@fundar.org.mx>
2014-10-23 18:06:10 -0500

    quitando mapa y tooltip

--- application/views/admin.php
+++ application/views/admin.php
@@ -9,8 +9,6 @@ foreach($css_files as $file): ?>
 <?php foreach($js_files as $file): ?>
 	<script src="<?php echo $file; ?>"></script>
 <?php endforeach; ?>
-<script src='https://api.tiles.mapbox.com/mapbox.js/v1.6.2/mapbox.js'></script>
-<link href='https://api.tiles.mapbox.com/mapbox.js/v1.6.2/mapbox.css' rel='stylesheet' />
 <style type='text/css'>
 	::selection{ background-color: #E13300; color: white; }
 	::moz-selection{ background-color: #E13300; color: white; }
@@ -68,8 +66,6 @@ foreach($css_files as $file): ?>
 		border: 1px solid #D0D0D0;
 		-webkit-box-shadow: 0 0 8px #D0D0D0;
 	}
-
-#map { position:relative; bottom:0; width:400px; height:400px; float:left; }
 </style>
 </head>
 <body>
@@ -80,42 +76,5 @@ foreach($css_files as $file): ?>
 			<?php echo $output; ?>
 		</div>
     </div>
-   
-    <script>
-		/*Tooltips*/
-		$(".tip-top").tooltip({placement : 'top'});
-		
-		/*Creaando el mapa de mapbox si esta presenta el div#map y el input#latitude*/
-		if(document.getElementById('map')) {
-			if(document.getElementById("field-latitude").value == "") {
-				var lat = 19.43268648150198;
-				var lng = -99.13318455219269;
-			} else {
-				var lat = document.getElementById("field-latitude").value;
-				var lng = document.getElementById("field-longitude").value;
-			}
-			
-			var map = L.mapbox.map('map', 'examples.map-20v6611k').setView([lat, lng], 8);
-			
-			if(document.getElementById('field-state')) {
-				var draggable = false;
-			} else {
-				var draggable = true;
-			}
-			
-			var marker = L.marker(new L.LatLng(lat, lng), {
-				icon: L.mapbox.marker.icon({'marker-color': 'CC0033'}),
-				draggable: draggable
-			});
-
-			marker.bindPopup('Mueve el marcador para ubicar al representante');
-			marker.addTo(map);
-			
-			marker.on('dragend', function(e){
-				document.getElementById("field-latitude").value  = e.target._latlng.lat;
-				document.getElementById("field-longitude").value = e.target._latlng.lng;
-			});
-		}
-	</script>
 </body>
 </html>
