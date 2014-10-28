5936f59ecc11f8a24b8ba656b3ef24142b9cc248
Carlos Hugo Gonzalez (Fundar) <carlos@fundar.org.mx>
2014-10-28 16:10:29 -0600

    fechas listado, header y votacion -  debug

--- application/models/curul501_model.php
+++ application/models/curul501_model.php
@@ -37,24 +37,31 @@ class curul501_Model extends CI_Model  {
 		$data  = $query->result_array();
 		
 		foreach($data as $value) {
-			//fecha_listado fecha_listado_header fecha_votacion
-			die(var_dump($value["fecha_listado_header"]));
-			
 			/*Fecha listado - presentada*/
 			$fecha_listado    = $value["fecha_listado"];
 			$fecha_listado    = str_replace(" de ", " ", $fecha_listado);
+			$fecha_listado    = str_replace(".", "", $fecha_listado);
+			$fecha_listado    = str_replace("+", "", $fecha_listado);
+			$fecha_listado    = str_replace("</li>", "", $fecha_listado);
+			$fecha_listado    = trim($fecha_listado);
 			$fecha_listado    = explode(" ", $fecha_listado);
 			$fecha_listado_tm = strtotime($fecha_listado[1] . '-' . $this->getMes(ucfirst($fecha_listado[2])) . '-' . $fecha_listado[3]);
 			var_dump(date("Y-m-d H:i:s", $fecha_listado_tm));
+			
+			/*Fecha listado header*/
+			$fecha_listado_header    = $value["fecha_listado_header"];
+			$fecha_listado_header    = str_replace(" de ", " ", $fecha_listado_header);
+			$fecha_listado_header    = trim($fecha_listado_header);
+			$fecha_listado_header    = explode(" ", $fecha_listado_header);
+			$fecha_listado_header_tm = strtotime($fecha_listado_header[1] . '-' . $this->getMes(ucfirst($fecha_listado_header[2])) . '-' . $fecha_listado_header[3]);
+			var_dump(date("Y-m-d H:i:s", $fecha_listado_header_tm));
 			
 			/*Fecha votacion*/
 			$fecha_votacion    = $value["fecha_votacion"];
 			$fecha_votacion    = str_replace(" de ", " ", $fecha_votacion);
 			$fecha_votacion    = explode(" ", $fecha_votacion);
-			$fecha_votacion_tm = strtotime($fecha_listado[0] . '-' . $this->getMes(ucfirst($fecha_listado[1])) . '-' . $fecha_listado[2]);
+			$fecha_votacion_tm = strtotime($fecha_votacion[0] . '-' . $this->getMes(ucfirst($fecha_votacion[1])) . '-' . $fecha_votacion[2]);
 			var_dump(date("Y-m-d H:i:s", $fecha_votacion_tm));
-
-			die("ok");
 		}
 	}
 	
