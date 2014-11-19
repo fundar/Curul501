bff287ec2249863be4aba8c3330abde679275c55
Carlos Hugo Gonzalez (Fundar) <carlos@fundar.org.mx>
2014-11-19 13:25:50 -0600

    add fields titulo_listado2 & html_listado2, unset fields admin

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -292,8 +292,7 @@ class Admin extends CI_Controller {
 			/*Columnas*/
 			$crud->columns('titulo_listado', 'titulo', 'presentada', 'fecha_listado_tm', 'commissions2initiatives', 'publicada');
 			//$crud->columns('id_initiative', 'id_legislature', 'titulo_listado', 'fecha_listado_tm', 'fecha_votacion_tm', 'periodo', 'presentada', 'commissions2initiatives', 'initiatives2topics', 'publicada');
-			$crud->unset_fields('id_parent', 'publicada', 'id_initiative', 'html_listado');
-			$crud->field_type('titulo_listado', 'invisible');
+			$crud->unset_fields('id_parent', 'publicada', 'id_initiative', 'titulo_listado2', 'html_listado2');
 			
 			/*custom action - publish*/
 			$crud->add_action('Publicar', '', '','ui-icon-plus',array($this, 'getUrlPublish'));
@@ -366,8 +365,7 @@ class Admin extends CI_Controller {
 			/*Columnas*/
 			$crud->columns('titulo_listado', 'titulo', 'presentada', 'fecha_listado_tm', 'commissions2initiatives');
 			//$crud->columns('id_initiative', 'id_legislature', 'titulo_listado', 'fecha_listado_tm', 'fecha_votacion_tm', 'periodo', 'presentada', 'commissions2initiatives', 'initiatives2topics', 'revisada');
-			$crud->unset_fields('id_parent', 'publicada', 'id_initiative', 'html_listado');
-			$crud->field_type('titulo_listado', 'invisible');
+			$crud->unset_fields('id_parent', 'publicada', 'id_initiative', 'titulo_listado2', 'html_listado2');
 			
 			/*Relaciones y displays*/
 			$crud->display_as('id_initiative', '#Iniciativa');
