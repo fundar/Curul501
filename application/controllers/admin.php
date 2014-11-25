66cfc26b4273928e4d492f368e0ad9016e1304bf
Carlos Hugo Gonzalez (Fundar) <carlos@fundar.org.mx>
2014-11-25 13:32:56 -0600

    votes - publish

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -469,9 +469,7 @@ class Admin extends CI_Controller {
 				$commissions = $this->curul501_model->getCommissionsByInitiative($id_initiative);
 				$status		 = $this->curul501_model->getStatusInitiative($id_initiative);
 				$votes		 = json_encode($this->curul501_model->getVotesPoliticalParties($id_initiative), JSON_NUMERIC_CHECK);
-				$votesRepresentantes = json_encode($this->curul501_model->getVotesRepresentatives($id_initiative), JSON_NUMERIC_CHECK);
-				
-				die(var_dump($votesRepresentantes));
+				$votesRepresentatives = json_encode($this->curul501_model->getVotesRepresentatives($id_initiative), JSON_NUMERIC_CHECK);
 				
 				/*topic*/
 				$string_topics		= "";
@@ -535,6 +533,8 @@ class Admin extends CI_Controller {
 					array('key' => 'wp_commissions_slug',  'value' => $string_commissions_slug),
 					array('key' => 'wp_presentada',	  	   'value' => $string_presentada),
 					array('key' => 'wp_presentada_slug',   'value' => $string_presentada_slug),
+					array('key' => 'wp_votes',					'value' => $votes),
+					array('key' => 'wp_votos_representantes',	'value' => $votesRepresentatives),
 					array('key' => 'wp_status',		  	   'value' => $string_status),
 					array('key' => 'wp_status_slug', 	   'value' => $string_status_slug)
 				);
