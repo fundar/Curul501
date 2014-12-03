60bebcc01bb2b6da04e85b0930d8e5ce346a6fd4
Carlos Hugo Gonzalez (Fundar) <carlos@fundar.org.mx>
2014-12-03 16:59:40 -0600

    Circunscripciónes

--- diagrama/distritos-geo/js/init-ubica.js
+++ diagrama/distritos-geo/js/init-ubica.js
@@ -1,4 +1,9 @@
 var sMarker = false;
+var primera = [2, 3, 8, 10, 14, 18, 25, 26];
+var segunda = [1, 5, 11, 19, 22, 24, 28, 32];
+var tercera = [4, 7, 20, 23, 27, 30, 31];
+var cuarta  = [9, 12, 17, 21, 29];
+var quinta  = [6, 13, 15, 16];
 
 function setMap() {
 	var map = L.map('map').setView([22.674847351188536, -101.77734374999999], 5);
@@ -10,10 +15,10 @@ function setMap() {
 			'Imagery © <a href="http://mapbox.com">Mapbox</a>',
 		id: 'examples.map-20v6611k'
 	}).addTo(map);
-
+	
 	function onEachFeature(feature, layer) {
 		layer.setStyle({
-			fillOpacity: 0, opacity: 0.1, weight: 1, color: "#432B4D", fillColor: "#3E2F52"
+			fillOpacity: 0, opacity: 0.1, weight: 1.2, color: "#432B4D", fillColor: "#3E2F52"
 		});
 		
 		layer.on('click', function(e) {
@@ -43,6 +48,18 @@ function getPip(lat, lng) {
 			var resultDisrtPip = leafletPip.pointInLayer([lng, lat], distritosLayer);
 			
 			if(resultDisrtPip.length) {
+				if(primera.indexOf(parseInt(resultPip[0].feature.properties.CVE_ENT)) != -1) {
+					console.log("Circunscripción: Primera");
+				} else if(segunda.indexOf(parseInt(resultPip[0].feature.properties.CVE_ENT)) != -1) {
+					console.log("Circunscripción: Segunda");
+				} else if(tercera.indexOf(parseInt(resultPip[0].feature.properties.CVE_ENT)) != -1) {
+					console.log("Circunscripción: Tercera");
+				} else if(cuarta.indexOf(parseInt(resultPip[0].feature.properties.CVE_ENT)) != -1) {
+					console.log("Circunscripción: Cuarta");
+				} else if(quinta.indexOf(parseInt(resultPip[0].feature.properties.CVE_ENT)) != -1) {
+					console.log("Circunscripción: Quinta");
+				}
+				
 				jQuery(".map-info-representante").html("Estado :" +  sMarker.options.NOMBRE +", Distrito: " + resultDisrtPip[0].feature.properties.DISTRITO);
 			} else {
 				console.log("Asegurate de estar en territorio mexicano");
