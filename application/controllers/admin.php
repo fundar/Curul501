dbbf5b7746f269ec4bbda72795d319dec3e38516
josechajob <josecha>
2015-10-09 17:50:32 -0500

    Revert "representantes"
    This reverts commit 6134c04ea4bdf91d7b8814563c32e0513c75ffce.

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -607,10 +607,10 @@ class Admin extends CI_Controller {
 					
 					if($ID) {
 						/*update publicada*/
-						//$this->curul501_model->setPublish($id_initiative);
+						$this->curul501_model->setPublish($id_initiative);
 						
 						/*set Initiative 2 Post*/
-						$this->curul501_model->setInitiative2Post($id_initiative, $ID);
+						//$this->curul501_model->setInitiative2Post($id_initiative, $ID);
 						
 						$response["success"] = true;
 						$response["ID"]      = $ID;
@@ -734,10 +734,10 @@ class Admin extends CI_Controller {
 					
 					if($ID) {
 						/*update publicada*/
-						//$this->curul501_model->setPublishRepresentative($id_representative);
+						$this->curul501_model->setPublishRepresentative($id_representative);
 						
 						/*set Representative 2 Post*/
-					$this->curul501_model->setRepresentative2Post($id_representative, $ID);
+						//$this->curul501_model->setRepresentative2Post($id_representative, $ID);
 						
 						$response["success"]   = true;
 						$response["ID"]        = $ID;
